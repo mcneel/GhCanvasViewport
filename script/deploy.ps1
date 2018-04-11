4d5128a0bd833f32d68ae1dd38daa2e48ea8adf1
Will Pearson <will@mcneel.com>
2018-04-11 16:12:24 +0100

    Add yak deploy script
    First crack at publishing prerelease packages from CI. Includes a baked
    in version of yak.exe and necessary libs (see 44e728a in
    mcneel/yak.net).
    
    See YAK-127

---
+++ script/deploy.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = "Stop" # exit on error
+
+# set version
+$file = 'dist\manifest.yml'
+$build = 1
+(Get-Content $file).replace('1.0.0-dev', "1.0.0-dev.$build") | Set-Content $file
+
+# copy .gha to dist/
+Copy-Item -Path bin\GhCanvasViewport.gha -Destination dist\
+
+# zip (create package manually)
+Compress-Archive -Path dist\* -DestinationPath dist\build.zip
+
+$filename = "build-$build.yak"
+Rename-Item -Path dist\build.zip -NewName $filename
+
+# publish
+.\tools\yak.exe push dist\$filename
