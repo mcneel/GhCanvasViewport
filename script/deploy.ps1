63c42cc117387a963f1cea332addbdb2f7c222b9
Will Pearson <will@mcneel.com>
2018-04-12 16:18:48 +0100

    Tidy up appveyor config and deploy script

--- script/deploy.ps1
+++ script/deploy.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop" # exit on error
 
 # set version
 $file = 'dist\manifest.yml'
-(Get-Content $file).replace('1.0.0-dev', "$env:YAK_PACKAGE_VERSION") | Set-Content $file
+(Get-Content $file).replace('1.0.0-dev', "$env:yak_package_version") | Set-Content $file
 
 # copy .gha to dist/
 Copy-Item -Path bin\GhCanvasViewport.gha -Destination dist\
@@ -11,9 +11,9 @@ Copy-Item -Path bin\GhCanvasViewport.gha -Destination dist\
 Compress-Archive -Path dist\* -DestinationPath dist\build.zip
 
 # rename file (unnecessary, but tidier)
-$filename = "build-$env:appveyor_build_number.yak"
-Rename-Item -Path dist\build.zip -NewName $filename
+# $filename = "build-$env:appveyor_build_number.yak"
+# Rename-Item -Path dist\build.zip -NewName $filename
 
 # publish
 .\tools\yak version
-.\tools\yak.exe push dist\$filename
+.\tools\yak.exe push dist\build.zip
